3259585c62383258c76d7b96e507938bb7384bbb
Pradip Thapa <thapapradip542@gmail.com>
2023-05-13 07:03:08 +0545

    update the code

--- core/settings/base.py
+++ core/settings/base.py
@@ -100,10 +100,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
