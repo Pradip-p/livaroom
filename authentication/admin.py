de5ee1e33e8b086cc29db9d0bd9406ef996aa298
Pradip Thapa <thapapradip542@gmail.com>
2023-05-15 13:02:41 +0545

    update the ci/cd

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import CustomUser
-# Register your models here.
-admin.site.register(CustomUser)
+from django.contrib.auth.models import Group
+admin.site.unregister(Group)
