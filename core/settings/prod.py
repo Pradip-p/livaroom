968e4dbfd2e2d4503817db1fda3fb820e5c61f5e
Pradip Thapa <thapapradip542@gmail.com>
2023-05-16 13:24:20 +0545

    update the database name

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -28,7 +28,7 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',#livaroom
+        'NAME': 'livaroomtools', #'myproject',#livaroom
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
