8e77cd2d4e5bbf5385ff1ecd6e51afcca9b3e491
Pradip Thapa <thapapradip542@gmail.com>
2023-05-17 09:46:09 +0545

    update the code

--- products/views.py
+++ products/views.py
@@ -12,11 +12,33 @@ from shopify import PaginatedIterator
 @login_required(login_url='/')
 def update_product_price(request):
     if request.method == "POST":
-        print('ok data is comming;.........')
-        skus = request.POST.getlist('skus[]')
-        print(skus)
-    print('get request iudjkbasdfvnkgedrs')
-    return JsonResponse({'message':''}, status=200)
+        prices = request.POST.getlist('prices[]')
+        # Update the value in the database or perform any other operations
+        API_KEY = "2b4e323d3129443363269802ebca49df"
+        API_ACCESS_TOKEN = "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"
+
+        SHOP_NAME = 'livaroom'
+
+        shop_url = f"https://{API_KEY}:{API_ACCESS_TOKEN}@{SHOP_NAME}.myshopify.com/admin/api/2023-01"
+        shopify.ShopifyResource.set_site(shop_url)
+        shop = shopify.Shop.current()
+        #now i want to update the prices of each prodcuts
+        # variant_sku = 'AR-9511'
+        for price in prices:
+            variant_sku = 'EEI-5805-CHE-WHI-WHI'
+            variant = Product.objects.get(sku=variant_sku)
+            if variant:
+                product_id = variant.product_id
+                variant_id = variant.variant_id
+                # Find the product variant based on SKU on Livaroom API(site)..
+                # product = shopify.Product(dict(id=product_id))
+                # variant = shopify.Variant(dict(id=variant_id, price=55.04)) #55.04
+                # product.add_variant(variant) #it does not mean add new varinat it update the existing price of variant.
+                # product.save()
+                # messages.success(request,'Price {} updated successfully.'.format(optimize_price))
+        return JsonResponse({'message': 'Price updated successfully.'}, status=200,)
+
+    return JsonResponse({'message':'invalid request.'}, status=500)
 
 
 @login_required(login_url='/')
@@ -94,7 +116,7 @@ def dashboard(request):
 def home(request):
     # #set the pagination on products li
     variants = [variant for variant in Product.objects.all().order_by('-id') if variant.price_englishelm]
-    # variants = Product.objects.all().order_by('-id')
+    variants = Product.objects.all().order_by('-id')
     variants = set_pagination(request, variants)
     context = {
                'variants':variants,
